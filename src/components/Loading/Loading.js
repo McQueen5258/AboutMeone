76464ae9e5ec0ae2ea0b6b69e016fe685071a663
Eric <49057152+McQueen5258@users.noreply.github.com>
2021-08-02 17:06:26 +0800

    Update Loading page

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './Loading.css';
 
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 export default class Loading extends Component {
     render() {
         return (
             <div className="loading">
+                <CircularProgress />
                 <h1> Loading. . . </h1>
             </div>
         );
